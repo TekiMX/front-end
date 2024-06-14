c25eb0fa557c2c4503c499c63c5812087f71cc2c
TekiMX <ale2ph@hotmail.com>
2024-06-13 22:25:18 -0600

    feature: creacion de primer componente ImageCards.jsx Ver.2

--- RE-04-giphy-app/src/App.jsx
+++ RE-04-giphy-app/src/App.jsx
@@ -1,6 +1,20 @@
+import { useState, useEffect } from "react";
 import './App.css'
 
 function App () {
+
+const [gifs, setGifs = useState ([])]
+
+// API calls happens when using useEffect normally
+useEffect(() => {
+  fetch('')
+  .then(res => res.json())
+  .then(results => {
+    const { data } = results
+    setGifs
+  })
+},[])
+
   return (
     <>
       <div>
