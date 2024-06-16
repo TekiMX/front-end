b2e2429692759d32800fdd4109783ff0fbd3e127
Salvador Rodriguez <ale2ph@hotmail.com>
2024-06-15 22:46:23 -0600

    feature: add basic SearchBar.jsx component

---
+++ RE-04-giphy-app/src/components/SearchBar.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+const SearchBar = ({ handleSearch }) => {
+  const [search, setSearch] = useState('')
+  return (
+    <div className='search-bar'>
+      <input
+        className='search-input'
+        type='text'
+        placeholder='Search'
+        onChange={(e) => { setSearch(e.target.value) }}
+        value={search}
+      />
+      <button
+        className='search-button'
+        onClick={handleSearch}
+      >
+        Buscar
+      </button>
+    </div>
+  )
+}
+
+export default SearchBar
