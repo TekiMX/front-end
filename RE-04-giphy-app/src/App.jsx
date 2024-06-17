8a852fe54d5cc1702edcdadbdbf0acc17ab268a0
Salvador Rodriguez <ale2ph@hotmail.com>
2024-06-16 22:49:05 -0600

    feature: terminado de proyecto con search bar incluido y funcionando

--- RE-04-giphy-app/src/App.jsx
+++ RE-04-giphy-app/src/App.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react'
 import ImageCards from './components/ImageCards'
+import SearchBar from './components/SearchBar'
 import './App.css'
 
 function App () {
   const [gifs, setGifs] = useState([])
   const KEY = import.meta.env.VITE_GIPHY_APP_KEY
+
+  const sendSearch = (search) => {
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${KEY}&q=${search}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
+      .then(res => res.json())
+      .then(results => {
+        const { data } = results
+        setGifs(data)
+      }).catch(err => console.log(err))
+  }
 
   // All API calls are used mostly inside useEffect hook
   useEffect(() => {
@@ -21,6 +31,7 @@ function App () {
     <>
       <div>
         <h1 className='title'>Re Giphy app</h1>
+        <SearchBar handleSearch={sendSearch} />
         <div className='grid-cards'>
           {
             gifs.map(gif => (
